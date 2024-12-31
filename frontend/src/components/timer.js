74b59ceceb5d539f905a698c6fae22e95f148531
Abhishek <abhishekmondal453@gmail.com>
2024-12-31 17:58:37 +0530

    good

--- frontend/src/components/timer.js
+++ frontend/src/components/timer.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import useInstance from "../hooks/useIntance"
 
 export const RegTimer = ({ deadline, compName }) => {
     const [days, setDays] = useState(0);
@@ -6,10 +7,9 @@ export const RegTimer = ({ deadline, compName }) => {
     const [minutes, setMinutes] = useState(0);
     const [seconds, setSeconds] = useState(0);
 
-    // Dummy function
-    const contestFunction = () => {
-        console.log("dummy function call");
-    };
+    const {instanceCreate} = useInstance();
+
+    // create a contest insatnce 30 min before the contest
 
     const getTime = (deadline) => {
         const time = Date.parse(deadline) - Date.now();
@@ -20,7 +20,7 @@ export const RegTimer = ({ deadline, compName }) => {
 
         // Check for the exact 30-minute mark
         if (days === 0 && hours === 0 && minutes === 30 && seconds === 0 && compName === "contest") {
-            contestFunction();
+            instanceCreate();
         }
     };
 
@@ -55,16 +55,23 @@ export const RegTimer = ({ deadline, compName }) => {
 };
 
 
-export const RunningTimer = ({ deadline }) => {
+export const RunningTimer = ({ deadline, compName}) => {
     const [hours, setHours] = useState(0);
     const [minutes, setMinutes] = useState(0);
     const [seconds, setSeconds] = useState(0);
+
+    const {instanceDelete} = useInstance();
 
     const getTime = (deadline) => {
         const time = Date.parse(deadline) - Date.now();
         setHours(Math.floor(time / (1000 * 60 * 60) % 24));
         setMinutes(Math.floor(time / (1000 * 60) % 60));
         setSeconds(Math.floor(time / (1000) % 60));
+
+        // Check for the ending of contest
+        if (hours === 0 && minutes === 0 && seconds === 0 && compName === "contest") {
+            instanceDelete();
+        }
     };
     useEffect(() => {
         const interval = setInterval(() => getTime(deadline), 1000);
